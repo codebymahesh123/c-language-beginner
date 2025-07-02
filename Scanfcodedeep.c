ad01bd85f3c892291e2b56868f670592ec2ac210
codebymahesh123 <maheshmau95@gmail.com>
2025-07-02 12:13:42 +0530

    Add files via upload

---
+++ Scanfcodedeep.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+
+    float a,b,c,d;
+    printf("enter the value is a = :");
+    
+    scanf("%f%f%f%f",&a,&b,&c,&d);
+   
+    printf("%f",a+b+c+d);
+    return 0;
+}
